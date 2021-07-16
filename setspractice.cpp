a4ec2263578f3e932a2cc1efbc2f252343055ef6
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-17 02:47:09 +0530

    Update setspractice.cpp

--- setspractice.cpp
+++ setspractice.cpp
@@ -10,7 +10,7 @@ int main() {
     s.insert("abc");//o(log(n)) insertion 
     s.insert("dfg");
     s.insert("tyh");
-    auto it=s.find("abcd");//o(log(n))
+    auto it=s.find("abcd");//o(log(n))//empty string will print in this.=s.end() hogya h
     if(it!=s.end())
         cout<<(*it)<<endl;
     
@@ -18,3 +18,27 @@ int main() {
 
 	
 }
+//////////////////////////////////////////////////////////////
+#include <bits/stdc++.h>
+using namespace std;
+void print(set<string>s){
+    for(auto string:s){
+        cout<<string<<endl;
+    }
+}
+int main() {
+	set<string>s;
+    s.insert("abc");//log(n) insertion 
+    s.insert("dfg");
+    s.insert("tyh");
+    s.insert("abc"); abc ek hi baar print hoga baar baar nhi 
+    print(s);
+ 
+/*
+abc print
+dfg
+tyh
+*/
+	
+}
+
