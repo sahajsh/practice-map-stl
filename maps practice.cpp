bc05433df874b113577fa8f5af9e7aa72e20eeaf
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-16 23:41:11 +0530

    Update maps practice.cpp

--- maps practice.cpp
+++ maps practice.cpp
@@ -59,12 +59,35 @@ void print(map<int,string> &m){
     }
 }
 int main() {
-     map<int ,string>m;
+    map<int ,string>m;
     m[3]="ansk";//insert krny ki time complexity o(log(n))
     m[2]="ass";
     m[8]="ass";
     auto it=m.find(2);// o(log(n)) time complexity
     m.erase(it);//o(log(n)) //iterator ki bhi values de skty h jis bhi pair ko point krrha hoga vo pair delete hojayega
+    print(m);
+  
+
+	return 0;
+}
+///////////////////////////////////////////////////
+#include <bits/stdc++.h>
+//agr 7 map m nhi h aur usko delete krna h---
+using namespace std;
+void print(map<int,string> &m){
+    cout<<m.size()<<endl;
+    for(auto &pr:m){
+        cout<<pr.first <<" "<<pr.second<<endl;
+    }
+}
+int main() {
+     map<int ,string>m;
+    m[3]="ansk";//insert krny ki time complexity o(log(n))
+    m[2]="ass";
+    m[8]="ass";
+    auto it=m.find(7);// o(log(n)) time complexity
+    if(it!=m.end())
+    m.erase(it);//o(log(n)) key value di h
     print(m);
   
 
