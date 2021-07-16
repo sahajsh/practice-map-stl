f38d176047d640004284ef438f3c7b61fcc7c28b
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-17 03:51:44 +0530

    Update mutliset.cpp

--- mutliset.cpp
+++ mutliset.cpp
@@ -22,3 +22,28 @@ abc
 abc
 dfg
 tyh
+////////////////////////////////////important erase function in multiset ///////////////////////////////////
+
+#include <bits/stdc++.h>
+using namespace std;
+void print(multiset<string>s){
+    for(auto value:s){
+        cout<<value<<endl;
+    }
+}
+int main() {
+	multiset<string>s;
+    s.insert("abc");//log(n) insertion 
+    s.insert("dfg");
+    s.insert("tyh");
+    s.insert("abc");
+    auto it=s.find("abc");
+    //if(it!=s.end()){
+    //    s.erase(it);//phly abc ka iterator return krega //and dusre abc ko delete krdega -- abc dfg tyh print hoga 
+    //}
+   s.erase("abc");//ye saare abc ko delete krdega
+    print(s);//phly abc ka iterator return krega find()
+    
+
+	
+}
